f0ca61ff4ada9ffcac305de4f0f7c53a69bac9c8
Wellux84 <jullesammakka@hotmail.fi>
2024-09-18 22:51:45 +0300

    4.3-4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,28 @@
+const dummy = (blogs) => {
+  return 1
+}
+  
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+
+  
+  const favorite = blogs.reduce((prev, current) => 
+    current.likes > prev.likes ? current : prev
+  )
+  
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  };
+};
+
+  module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+  }
+
